7966684077398862e50324299b69fda828e49ba7
Babaprakash D <babaprakash.dabbara@ikea.com>
2019-07-22 16:51:06 +0200

    Config Server

---
+++ src/test/java/configuration/cloudconfig/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package configuration.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
